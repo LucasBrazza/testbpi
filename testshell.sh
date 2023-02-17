e96dcd0f094f015da243119a40f64161155db8e7
Lucas Brazzarola <69698754+LucasBrazza@users.noreply.github.com>
2023-02-17 14:46:21 -0300

    Update testshell.sh

--- testshell.sh
+++ testshell.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-git pull
-sudo armbian-config
+sudo apt-get install build-essential libreadline-dev libffi-dev pkg-config gcc-arm-none-eabi libnewlib-arm-none-eabi
+git clone --recurse-submodules https://github.com/micropython/micropython.git
+cd micropython/mpy-cross
+make
