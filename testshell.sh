f7a62066b9bd4529881ad692f9fcec4586afcc5f
Lucas Brazzarola <69698754+LucasBrazza@users.noreply.github.com>
2023-04-17 16:49:19 -0300

    Update testshell.sh

--- testshell.sh
+++ testshell.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+sudo apt-get update
+sudo apt-get install openmpi-bin libopenmpi-dev
+
+
+
 sudo apt-get install build-essential libreadline-dev libffi-dev pkg-config gcc-arm-none-eabi libnewlib-arm-none-eabi
 git clone --recurse-submodules https://github.com/micropython/micropython.git
 cd micropython/mpy-cross
